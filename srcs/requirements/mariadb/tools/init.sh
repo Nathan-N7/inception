5ef671243af251bdd08a25aa8a6a7ebc7763ece1
Nathan-N7 <nathan.rod1710@gmail.com>
2026-03-27 20:09:36 -0300

    comments mariadb

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+
+#stop command fail
 set -e
 
 mkdir -p /run/mysqld
+#permission for user
 chown -R mysql:mysql /run/mysqld
 DB_PASSWORD=$(cat /run/secrets/db_password)
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
+#if the bank doesn't exist
+#creat bank
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
     mysqld --user=mysql --bootstrap << EOF
-FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
-FLUSH PRIVILEGES;
+    FLUSH PRIVILEGES;
+    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
 EOF
 fi
 
